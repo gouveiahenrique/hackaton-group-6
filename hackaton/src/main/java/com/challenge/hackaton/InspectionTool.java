86225d58206bc3e772495b9bf2cf6d279e5631d7
Leonardo.Fernandes-ext <leonardo.fernandes-ext@ab-inbev.com>
2023-08-03 13:38:44 -0300

    Initiate inspectionTool

--- hackaton/src/main/java/com/challenge/hackaton/InspectionTool.java
+++ hackaton/src/main/java/com/challenge/hackaton/InspectionTool.java
@@ -1,7 +1,13 @@
 package com.challenge.hackaton;
 
+import com.intellij.codeHighlighting.HighlightDisplayLevel;
 import com.intellij.codeInspection.LocalInspectionTool;
+import org.jetbrains.annotations.NotNull;
 
 public class InspectionTool extends LocalInspectionTool {
 
+    @Override
+    public @NotNull HighlightDisplayLevel getDefaultLevel() {
+        return super.getDefaultLevel();
+    }
 }
